bd3eb2c9fbc60cdfe515ab881c3338a6c5dc63bb
Conejo021 <gustavoconiglio@gmail.com>
2021-07-19 00:27:11 -0300

    commit corregido

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const app = express();
-app.listen(process.env.PORT||3000, ()=>{
-    console.log('Servidor Corriendo');
+app.use(express.static('public'));
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Servidor funcionando en el puerto 3000');
 });
 
-app.get('/',(req,res)=>{
-    res.sendFile(_dirname +'/views/home.html');
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
 });
-app.get('/',(req,res)=>{
-  res.sendFile(_dirname +'/views/login.html');
+
+app.get('/login', (req,res)=>{
+    res.sendFile(__dirname + '/views/login.html');
 });
-app.get('/',(req,res)=>{
-    res.sendFile(_dirname +'/views/register.html');
+
+app.get('/register', (req,res)=>{
+    res.sendFile(__dirname + '/views/register.html');
 });
